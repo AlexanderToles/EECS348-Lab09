c255e6f54f9524fb6f624f0c73e6d50dad5b6a16
Alexnder Toles <a217t230@ku.edu>
2025-04-08 13:00:10 -0500

    updated classes based off template

---
+++ matrix.cpp
@@ -0,0 +1,46 @@
+#include "matrix.hpp"
+
+// implementation of functions declared in matrix.hpp goes here
+// function definitions for a class have their names prefixed with
+// the class name and "::"
+
+// e.g. for the constructor:
+
+Matrix::Matrix(std::size_t N) {
+}
+
+Matrix::Matrix(std::vector<std::vector<int>> nums){
+
+}
+
+// e.g. for a member function:
+Matrix operator+(const Matrix &rhs) const{
+
+}
+Matrix operator*(const Matrix &rhs) const{
+
+}
+void set_value(std::size_t i, std::size_t j, int n){
+
+}
+int get_value(std::size_t i, std::size_t j) const{
+    return 0;
+}
+int get_size() const{
+    return 0;
+}
+int sum_diagonal_major() const{
+    return 0;
+}   
+int sum_diagonal_minor() const{
+    return 0;
+}
+void swap_rows(std::size_t r1, std::size_t r2){
+
+}
+void swap_cols(std::size_t c1, std::size_t c2){
+
+}
+void print_matrix() const{
+
+}
