4adb886459d4054fe8eaaa35fe303cde7d45a3ae
Alexnder Toles <a217t230@ku.edu>
2025-04-08 14:51:25 -0500

    added more functions, implemented matrix into problem.cpp

--- Makefile
+++ Makefile
@@ -1,11 +1,3 @@
-CC=g++
-CFLAGS = -c -Wall
-problem1.exe: problem1.o matrix.o
-	$(CC) problem1.o matrix.o -o problem1.exe
-problem1.o: problem1.cpp matrix.hpp
-	$(CC) $(CFLAGS) problem1.cpp -o problem1.o
-matrix.o: matrix.cpp matrix.hpp
-	$(CC) $(CFLAGS) matrix.cpp -o matrix.o
-clean:
-	rm -rf *.o
-	rm -rf *.exe
+problem1: problem1.cpp
+	g++ problem1.cpp -o problem1
+	./problem1
--- problem1.cpp
+++ problem1.cpp
@@ -2,12 +2,61 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <regex>
+
 
 using namespace std;
 
+template <typename T> class Matrix {
+    private:
+        int size;
+        vector<vector<T>> data;
+    
+    public:
+        Matrix(size_t N);
+        Matrix(vector<vector<T>> nums);
+    
+        Matrix operator+(const Matrix &rhs) const;
+        Matrix operator*(const Matrix &rhs) const;
+        void set_value(size_t i, size_t j, T n);
+        T get_value(size_t i, size_t j) const;
+        int get_size() const;
+        T sum_diagonal_major() const;
+        T sum_diagonal_minor() const;
+        void swap_rows(size_t r1, size_t r2);
+        void swap_cols(size_t c1, size_t c2);
+        void print_matrix() const;
+    };
+
+void readData(vector<string> fileData, int start, int end){
+    for(int i = start; i < end; i++){
+        smatch match;
+        cout << "line: " << i << "\n" ;
+        regex searchTerm("(\\d+)");
+        auto lineStart = sregex_iterator(fileData[i].begin(),fileData[i].end(),searchTerm);
+        auto lineEnd = sregex_iterator();
+        for(sregex_iterator i = lineStart; i != lineEnd; ++i){
+            smatch match = *i;
+            cout << match.str() << "\n";
+        }
+    }
+
+}
+
 void scanFile(vector<string> fileData){
     for(int i = 0; i < fileData.size(); i++){
-        cout << fileData[i] << "\n";
+        smatch match;
+        int size; 
+        int type;
+        if(regex_search(fileData[i],match,regex ("^(\\d+)\\s(\\d)")) && i==0){
+            size = stoi(match[1].str());
+            type = stoi(match[2].str());
+        }
+        if(fileData[i]!= "" && i>0){
+            readData(fileData, i, i+size);
+            break;
+        }
+        //cout << fileData[i] << "\n";
     }
 }
 
