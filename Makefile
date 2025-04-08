fe57202e88f6641089d6a9855499bbf71597b878
Alexnder Toles <a217t230@ku.edu>
2025-04-08 17:05:29 -0500

    updated comments and makefile

--- Makefile
+++ Makefile
@@ -1,3 +1,2 @@
 problem1: problem1.cpp
 	g++ problem1.cpp -o problem1
-	./problem1
--- problem1.cpp
+++ problem1.cpp
@@ -1,3 +1,12 @@
+//  EECS 348 LAB 09
+//  MATRIX OPERATIONS
+//  INPUT: Test file, menu selection
+//  OUTPUT: Menu, matricies
+//  ALEXANDER TOLES      
+//  APRIL 08 2025
+
+
+
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -7,13 +16,13 @@
 
 using namespace std;
 
-template <typename T> class Matrix {
-    private:
-        int size;
-        vector<vector<T>> data;
+template <typename T> class Matrix { //Matrix class definition, using template, need to pass in Matrix type during declaration
+    private: //variables used in matrix functions
+        int size; 
+        vector<vector<T>> data; //2d vector of passed in values
     
     public:
-        Matrix(vector<vector<T>> nums){
+        Matrix(vector<vector<T>> nums){ //constructor, takes in a 2d vector of numbers
             data = nums;
             size = nums.size();
         }
@@ -68,7 +77,7 @@ template <typename T> class Matrix {
             return(sum);
         }
         void swapRows(std::size_t r1, std::size_t r2){
-            vector<T> tempRow = data[r1];
+            vector<T> tempRow = data[r1]; //set tempory val
             data[r1] = data[r2];
             data[r2] = tempRow;
             printMatrix();
@@ -83,7 +92,7 @@ template <typename T> class Matrix {
             printMatrix();
         }
         void printMatrix() const{
-            for(int i = 0; i < size; ++i){
+            for(int i = 0; i < size; ++i){ //go through each row, printing each column to the screen
                 for(int j = 0; j < size; ++j){
                     cout << data[i][j] << " ";
                 }
@@ -183,22 +192,29 @@ void printMenu(Matrix<T> &matrixA,Matrix<T> &matrixB){
         }
     }
 }
-vector<vector<string>> readData(vector<string> fileData, int start, int end, int type ){
 
-    vector<vector<string>> returnData;
+//read the data from the line, takes a starting line postion and end position, indexing over
+//each line and adding it to a 2d vector
 
-    for(int i = start; i < end; i++){
+vector<vector<string>> readData(vector<string> fileData, int start, int end, int type ){ 
+
+    vector<vector<string>> returnData; //2d vector of strings, gets translated in compileMatricies() function
+
+    for(int i = start; i < end; i++){ //go through each line from the start of the matrix to the end
 
         vector<string> row;
         regex searchTerm;
         
-        if(type == 1){
-            searchTerm = ("(\\d+[.]\\d+)");
+        if(type == 1){ //regex search changes depending on double or int
+            searchTerm = ("(\\d+[.]\\d+)"); //need the number before and after, and the decimal
         } else {
-            searchTerm = ("(\\d+)");
+            searchTerm = ("(\\d+)"); //just need the numbers
         }
         
-        auto lineStart = sregex_iterator(fileData[i].begin(),fileData[i].end(),searchTerm);
+        //setup sregex_iterator, this bit parses through each regex match in the line, apending them to the
+        //returnData
+
+        auto lineStart = sregex_iterator(fileData[i].begin(),fileData[i].end(),searchTerm); 
         auto lineEnd = sregex_iterator();
         for(sregex_iterator i = lineStart; i != lineEnd; ++i){
             smatch match = *i;
@@ -210,27 +226,38 @@ vector<vector<string>> readData(vector<string> fileData, int start, int end, int
     return returnData;
 
 }
+
+
+//compileMatricies Takes in the data as strings, and then translates the data based on the matrix type, then creates
+//two matrix objects for the matricies. Finally, it calls the printMenu function.
+
 void compileMatricies(vector<vector<string>> matrixAData, vector<vector<string>> matrixBData, int size, int type){
     
-    if(type==0){
+    if(type==0){ //if matrix type is integer
+
+        //create two 2d vectors to store the translated data
 
         vector<vector<int>> convertAData(size,vector<int>(size));
         vector<vector<int>> convertBData(size,vector<int>(size));
-
+        //goes through each entry in the input data, translates it, and adds it to the converted data
         for(int i = 0; i < size; ++i){
             for(int j = 0; j < size; ++j){
                 convertAData[i][j] = stoi(matrixAData[i][j]);
                 convertBData[i][j] = stoi(matrixBData[i][j]);
             }
         }
+
+        //create matricies using the converted data
         Matrix<int> matrixA(convertAData);
         Matrix<int> matrixB(convertBData);
 
         matrixA.printMatrix();
         matrixB.printMatrix();
+
+        //call printMenu with the matricies
         printMenu(matrixA,matrixB);
     }
-    else{
+    else{ //same thing but with double, now that i think of it, probably coulda used a template here but im tired
         vector<vector<double>> convertAData(size,vector<double>(size));
         vector<vector<double>> convertBData(size,vector<double>(size));
 
@@ -249,24 +276,20 @@ void compileMatricies(vector<vector<string>> matrixAData, vector<vector<string>>
     }
     
 }
+
+//scan through each line
 void scanFile(vector<string> fileData){
-    for(int i = 0; i < fileData.size(); i++){
-        smatch match;
+    for(int i = 0; i < fileData.size(); i++){ //iterate through each line
+        smatch match; //variable to store matches
         int size; 
         int type;
-        if(regex_search(fileData[i],match,regex ("^(\\d+)\\s(\\d)")) && i==0){
+        if(regex_search(fileData[i],match,regex ("^(\\d+)\\s(\\d)")) && i==0){ //search for the first two numbers
             size = stoi(match[1].str());
             type = stoi(match[2].str());
         }
-        if(fileData[i]!= "" && i>0){
+        if(fileData[i]!= "" && i>0){ //find start of matrixA, read the data, pass the data to compileMatrices
             vector<vector<string>> matrixAData = readData(fileData, i, i+size, type);
-            vector<vector<string>> matrixBData = readData(fileData, i+size, i+size+size, type);
-            compileMatricies(matrixAData,matrixBData,size,type);
-            break;
-        }
-        if(fileData[i]!= "" && i>0){
-            vector<vector<string>> matrixAData = readData(fileData, i, i+size, type);
-            vector<vector<string>> matrixBData = readData(fileData, i+size, i+size+size, type);
+            vector<vector<string>> matrixBData = readData(fileData, i+size, i+size+size, type); //call from matrixB start position
             compileMatricies(matrixAData,matrixBData,size,type);
             break;
         }
@@ -274,16 +297,16 @@ void scanFile(vector<string> fileData){
 }
 
 
-bool verify(string fileName){
+bool verify(string fileName){ //check if the input file is able to be opened
     fstream inputFile;
     inputFile.open(fileName, fstream::in);
     if(inputFile.is_open()){
         string line;
         vector<string> fileData;
-        while(getline(inputFile,line)){
+        while(getline(inputFile,line)){ //create a vector containing each of the lines
             fileData.push_back(line);
         }
-        scanFile(fileData);    
+        scanFile(fileData);    //pass vector to the scanFile function
         return 1;
     }
     else{
@@ -292,12 +315,25 @@ bool verify(string fileName){
     }
 }
 
+
+//Matrix program flow:
+//1) get file from user
+//2) read the data from file
+//3) create two 2d vectors containing the data as strings
+//4) compileMatricies translates the data and calls print menu
+//5) printMenu handles user interaction
+
+
+
+
+
+
 int main() {
-    while(true){
+    while(true){ //prompt user for input file name until valid name is chosen
         string fileName;
         cout << "Enter file name: ";
         cin >> fileName;
-        if(verify(fileName)){
+        if(verify(fileName)){ //check if file was able to be opened
             break;
         }
     }
