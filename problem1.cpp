aafe6b1a2bab8f7f50119b3c3bc465cc28e04ff7
Alexnder Toles <a217t230@ku.edu>
2025-04-08 15:43:30 -0500

    updated matrix creation function

--- problem1.cpp
+++ problem1.cpp
@@ -13,36 +13,70 @@ template <typename T> class Matrix {
         vector<vector<T>> data;
     
     public:
-        Matrix(size_t N);
-        Matrix(vector<vector<T>> nums);
-    
-        Matrix operator+(const Matrix &rhs) const;
-        Matrix operator*(const Matrix &rhs) const;
-        void set_value(size_t i, size_t j, T n);
-        T get_value(size_t i, size_t j) const;
-        int get_size() const;
-        T sum_diagonal_major() const;
-        T sum_diagonal_minor() const;
-        void swap_rows(size_t r1, size_t r2);
-        void swap_cols(size_t c1, size_t c2);
-        void print_matrix() const;
-    };
+        Matrix(vector<vector<T>> nums){
+            data = nums;
+            size = nums.size();
+        }
+        void print_matrix() const{
+            cout << "Print matrix called \n";
+            for(int i = 0; i < size; ++i){
+                for(int j = 0; j < size; ++j){
+                    cout << data[i][j] << " ";
+                }
+                cout << "\n";
+            }
+        }
+};
 
-void readData(vector<string> fileData, int start, int end){
+vector<vector<string>> readData(vector<string> fileData, int start, int end, int type ){
+
+    vector<vector<string>> returnData;
+
     for(int i = start; i < end; i++){
-        smatch match;
+
+        vector<string> row;
+
         cout << "line: " << i << "\n" ;
-        regex searchTerm("(\\d+)");
+        regex searchTerm;
+        
+        if(type == 1){
+            searchTerm = ("(\\d+[.]\\d+)");
+        } else {
+            searchTerm = ("(\\d+)");
+        }
+        
         auto lineStart = sregex_iterator(fileData[i].begin(),fileData[i].end(),searchTerm);
         auto lineEnd = sregex_iterator();
         for(sregex_iterator i = lineStart; i != lineEnd; ++i){
             smatch match = *i;
+            row.push_back(match.str());
             cout << match.str() << "\n";
         }
+        returnData.push_back(row);
     }
+    
+    return returnData;
 
 }
+void compileMatricies(vector<vector<string>> matrixAData, vector<vector<string>> matrixBData, int size, int type){
+    
+    if(type==0){
+        vector<vector<int>> convertAData(size,vector<int>(size));
+        vector<vector<int>> convertBData(size,vector<int>(size));
 
+        for(int i = 0; i < size; ++i){
+            for(int j = 0; j < size; ++j){
+                convertAData[i][j] = stoi(matrixAData[i][j]);
+                convertBData[i][j] = stoi(matrixBData[i][j]);
+            }
+        }
+        Matrix<int> matrixA(convertAData);
+        Matrix<int> matrixB(convertBData);
+
+        matrixA.print_matrix();
+        matrixB.print_matrix();
+    }
+}
 void scanFile(vector<string> fileData){
     for(int i = 0; i < fileData.size(); i++){
         smatch match;
@@ -53,10 +87,11 @@ void scanFile(vector<string> fileData){
             type = stoi(match[2].str());
         }
         if(fileData[i]!= "" && i>0){
-            readData(fileData, i, i+size);
+            vector<vector<string>> matrixAData = readData(fileData, i, i+size, type);
+            vector<vector<string>> matrixBData = readData(fileData, i+size, i+size+size, type);
+            compileMatricies(matrixAData,matrixBData,size,type);
             break;
         }
-        //cout << fileData[i] << "\n";
     }
 }
 
